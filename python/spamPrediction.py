0ed4a223b9f249bcb77c2902ed0c509f02fa59fa
Bríd O'Donnell <odonneb4@tcd.ie>
2023-02-14 17:13:03 +0000

    Debugged spam Prediction script

--- python/spamPrediction.py
+++ python/spamPrediction.py
@@ -5,7 +5,6 @@ import pickle
 message = sys.argv[1]
 pickled_vectorizer = pickle.load(open('python/vectorizer.pkl', 'rb'))
 messageList = [message]
-print(type(messageList))
 X = pickled_vectorizer.transform(messageList)
 pickled_model = pickle.load(open('python/model.pkl', 'rb'))
 print("If 0, message is spam, if 1, message is real disaster...")
