dac7c220eb78be81fe956134c83961d42ea38e7d
Bríd O'Donnell <odonneb4@tcd.ie>
2023-03-28 13:59:19 +0100

    Added TryThis.js file for quick testing of the allocator

--- logic/TryThis.js
+++ logic/TryThis.js
@@ -1,5 +1,18 @@
-/* This file is to manually test the scheduler function. */
+const {allocateResources} = require("../logic/ResourceAllocator")
+const disasterTypeObj = require('../logic/DisasterInterpretation.js');
+const disasterLocationObj = require('../logic/ImpactRadiusInterpretation.js')
+const disasterSizeObj = require('../logic/ImpactSizeInterpretation.js')
 
-const {processOldReports} = require('./Scheduler');
-
-processOldReports();
+const test = "fire in the building";
+disasterType = disasterTypeObj.interpretDisaster(test);
+disasterLocation = disasterLocationObj.interpretDisasterLocation(test);
+disasterRadius = disasterLocationObj.interpretDisasterRadius(disasterType, disasterLocation);
+disasterImpactedPeopleCount = disasterSizeObj.interpretImpactSize(test, disasterLocation);
+console.log(`Disaster Type:${disasterType}`);
+console.log(`Disaster Location:${disasterLocation}`);
+console.log(`Disaster Radius:${disasterRadius}`);
+console.log(`Disaster Impact:${disasterImpactedPeopleCount}`);
+let interpretation = [disasterType,disasterLocation,disasterRadius,disasterImpactedPeopleCount];
+console.log(interpretation);
+const resources = allocateResources(interpretation); 
+console.log(resources);
