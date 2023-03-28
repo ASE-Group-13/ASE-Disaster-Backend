c68f7e5cbce893d3b04c5d2e26b6f354deec8052
Bríd O'Donnell <odonneb4@tcd.ie>
2023-03-28 15:59:40 +0100

    Added DT ML model

--- logic/TryThis.js
+++ logic/TryThis.js
@@ -1,9 +1,11 @@
-const {allocateResources} = require("../logic/ResourceAllocator")
+const {allocateResources, trainModel} = require("../logic/ResourceAllocator")
 const disasterTypeObj = require('../logic/DisasterInterpretation.js');
 const disasterLocationObj = require('../logic/ImpactRadiusInterpretation.js')
 const disasterSizeObj = require('../logic/ImpactSizeInterpretation.js')
 
-const test = "fire in the building";
+// trainModel();
+
+const test = "fire in the building, many people are injured";
 disasterType = disasterTypeObj.interpretDisaster(test);
 disasterLocation = disasterLocationObj.interpretDisasterLocation(test);
 disasterRadius = disasterLocationObj.interpretDisasterRadius(disasterType, disasterLocation);
@@ -12,7 +14,7 @@ console.log(`Disaster Type:${disasterType}`);
 console.log(`Disaster Location:${disasterLocation}`);
 console.log(`Disaster Radius:${disasterRadius}`);
 console.log(`Disaster Impact:${disasterImpactedPeopleCount}`);
-let interpretation = [disasterType,disasterLocation,disasterRadius,disasterImpactedPeopleCount];
+let interpretation = [[0,0,disasterRadius,disasterImpactedPeopleCount]];
 console.log(interpretation);
 const resources = allocateResources(interpretation); 
 console.log(resources);
--- python/scripts/resourceAllocation.py
+++ python/scripts/resourceAllocation.py
@@ -4,14 +4,11 @@ import os
 import pandas as pd
 
 
-pickled_encoder = pickle.load(open('./python/models/encoder.pkl', 'rb'))
+# pickled_encoder = pickle.load(open('./python/models/encoder.pkl', 'rb'))
 # Make predictions on new data
-new_data = pd.DataFrame({
-    "Disaster Type": [sys.argv[0]],
-    "Impact Place": [sys.argv[1]],
-    "Disaster Size in Meters": [sys.argv[2]],
-    "Impact Size in persons": [sys.argv[3]]
-})
-new_data_encoded = pickled_encoder.transform(new_data)
-pickled_model = pickle.load(open('./python/models/disaster_model.pkl', 'rb'))
-print(pickled_model.predict(new_data_encoded)[0])
+new_data = [[int(num) for num in sys.argv[1].split(",")]]
+# new_data_encoded = pickled_encoder.transform(new_data)
+pickled_model = pickle.load(open('./python/models/disaster_model_dt.pkl', 'rb'))
+prediction = pickled_model.predict(new_data)
+print(prediction)
+
